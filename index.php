73d416331c5c07dbd8651b6d4b8217d94b65a4b7
Brian Rakhmat Aji <brianetlab@gmail.com>
2023-03-04 13:34:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! Declarative pipeline!! Halo</p>'; ?> 
+ <?php echo '<p>Hello world from PHP! Declarative pipeline!! Hola</p>'; ?> 
  </body>
 </html>
